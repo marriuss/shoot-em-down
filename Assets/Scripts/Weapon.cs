f1db8ca8e68f62aa44090109ab23c10cf743bfce
marriuss <53435883+marriuss@users.noreply.github.com>
2022-10-31 22:20:16 +0400

    Level generation started

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerShooter))]
+[RequireComponent(typeof(Rigidbody))]
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Transform _bulletSpawnPoint;
+
+    private const float KnockbackStrength = 7;
+
+    private PlayerShooter _playerShooter;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _playerShooter = GetComponent<PlayerShooter>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        _playerShooter.PlayerShot += OnPlayerShot;
+    }
+
+    private void OnDisable()
+    {
+        _playerShooter.PlayerShot -= OnPlayerShot;
+    }
+
+    private void OnPlayerShot()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        Vector3 bulletSpawnPointPosition = _bulletSpawnPoint.position;
+        Vector3 knockbackDirection = (bulletSpawnPointPosition - _rigidbody.position).normalized;
+        _rigidbody.AddForceAtPosition(-knockbackDirection * KnockbackStrength, bulletSpawnPointPosition, ForceMode.VelocityChange);
+    }
+}
