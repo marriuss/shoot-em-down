7fd256364e8ec6b20345db56838cfa3f4302ed70
marriuss <53435883+marriuss@users.noreply.github.com>
2022-12-01 12:45:16 +0400

    Added adds support

---
+++ Assets/Source/Scripts/Yandex/Ads/FullscreenVideo.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Agava.YandexGames;
+
+public class FullscreenVideo : MonoBehaviour
+{
+    [SerializeField, Min(1)] private int _cooldown;
+    [SerializeField] private GameAudio _audio;
+
+    private int _menuCount;
+
+    private void Awake()
+    {
+        _menuCount = 0;
+    }
+
+    public void Show()
+    {
+        if (_menuCount == _cooldown)
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        InterstitialAd.Show(
+            onOpenCallback: () => _audio.TurnOffMusic(),
+            onCloseCallback: (bool _) => _audio.TurnOnMusic()
+        );
+#endif
+            _menuCount = 0;
+        }
+        else
+        {
+            _menuCount++;
+        }
+    }
+}
